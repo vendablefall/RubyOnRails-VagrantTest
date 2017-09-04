e290e1a7fdc07be60189960c88d35581021b0960
VendableFall <info@leet.cloud>
2017-09-04 15:26:59 +1000

    rvm install

---
+++ rvm-install.sh
@@ -0,0 +1,8 @@
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${YELLOW}get gpg key ${NC}"
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+\curl -sSL https://get.rvm.io | bash -s stable 
