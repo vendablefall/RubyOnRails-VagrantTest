50a4def12b3d1c2e46c17078d18f308d20e45fea
VendableFall <info@leet.cloud>
2017-09-04 15:55:01 +1000

    rvm 2.2.5 working + formatting in .sh files

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@ NC='\033[0m'
 
 echo -e "${YELLOW}apt-get update ${NC}"
 apt-get update
+
+echo -e "${YELLOW}apt-get install curl ${NC}"
+
 apt-get install -y curl
 
 mkdir /projects/
--- rvm-setup.sh
+++ rvm-setup.sh
@@ -1,14 +1,21 @@
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
 source /usr/local/rvm/scripts/rvm
+
 echo -e "${YELLOW} rvm install 2.2.5 ${NC}"
+
 rvm install 2.2.5 
+
 echo -e "${YELLOW} rvm use 2.2.5 ${NC}"
+
 rvm use 2.2.5
-sleep 2
+#sleep 2
 echo -e "${YELLOW} gem install rails ${NC}"
-cd /projects/
+#cd /projects/
 
 gem install rails
+
 echo -e "${YELLOW} gem install bundler ${NC}"
+
 gem install bundler
