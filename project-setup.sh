bc973de90c148acc4efafad2acf0fe97a6a36e1e
VendableFall <info@leet.cloud>
2017-09-04 17:03:51 +1000

    generate controller aticles

--- project-setup.sh
+++ project-setup.sh
@@ -17,6 +17,11 @@ bundle install --path /projects/
 
 #http://guides.rubyonrails.org/getting_started.html
 
+echo -e "${YELLOW} copying new routes.rb ${NC}"
+
+rm -f /projects/testapp/config/routes.rb
+cp /vagrant/files/routes.rb /projects/testapp/config/
+
 echo -e "${YELLOW} generate controller Articles ${NC}"
 
-bin/rails generate controller Welcome index
+bin/rails generate controller Articles
